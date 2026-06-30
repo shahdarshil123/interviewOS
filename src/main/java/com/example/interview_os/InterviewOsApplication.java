9e01d35439620d02e8c599561d962ccbf61bda52
shahdarshil123 <darshilshah622@gmail.com>
2026-06-30 15:29:31 -0400

    project setup, Postgres connection working

---
+++ src/main/java/com/example/interview_os/InterviewOsApplication.java
@@ -0,0 +1,13 @@
+package com.example.interview_os;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewOsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/interview_os/InterviewOsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interview_os;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewOsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
